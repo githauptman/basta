3583351363ea764ab791b4804918e8fdff700749
hauptimus <hauptman24@gmail.com>
2020-04-28 15:07:11 -0600

    Added Franchises

--- basta.py
+++ basta.py
@@ -12,6 +12,18 @@ class Menu():
             calculated_bill +=  self.items[item]
         return calculated_bill
 
+class Franchise():
+    def __init__(self, address, menus):
+        self.address = address
+        self.menus = menus
+    def __repr__(self):
+        return self.address
+    def available_menus(self, time):
+        available_menus = []
+        for menu in self.menus:
+            if time > menu.start_time and time < menu.end_time:
+                available_menus.append(menu.name)
+        return available_menus
 
 brunch_items = {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
@@ -34,5 +46,16 @@ early_bird_menu = Menu('Early-Bird', early_bird_items, 15, 18)
 dinner_menu = Menu('Dinner', dinner_items, 17, 23)
 kids_menu = Menu('Kids', kids_menu_items, 11, 21)
 
+"""
+#print calculate bill function
 print(brunch_menu.calculate_bill(['pancakes', 'home fries','coffee']))
 print(early_bird_menu.calculate_bill(['salumeria plate', 'mushroom ravioli (vegan)']))
+"""
+
+menus = [brunch_menu, early_bird_menu, dinner_menu, kids_menu]
+
+flagship_store = Franchise("1232 West End Road", menus)
+new_installment = Franchise("12 East Mulberry Stree", menus)
+
+#print(flagship_store.available_menus(14))
+
