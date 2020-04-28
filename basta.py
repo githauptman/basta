ed4edcc7ded243d8342a9d1af2c060dbd6d3f0c6
hauptimus <hauptman24@gmail.com>
2020-04-28 15:26:14 -0600

    Added Franchises

--- basta.py
+++ basta.py
@@ -25,6 +25,11 @@ class Franchise():
                 available_menus.append(menu.name)
         return available_menus
 
+class Business():
+    def __init__(self, name, franchises):
+        self.name = name
+        self.franchises = franchises
+
 brunch_items = {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
 }
@@ -41,10 +46,15 @@ kids_menu_items = {
   'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
 }
 
+arepas_menu_items = {
+  'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50
+}
+
 brunch_menu = Menu('Brunch', brunch_items, 11, 16)
 early_bird_menu = Menu('Early-Bird', early_bird_items, 15, 18)
 dinner_menu = Menu('Dinner', dinner_items, 17, 23)
 kids_menu = Menu('Kids', kids_menu_items, 11, 21)
+arepas_menu = Menu('Arepas', arepas_menu_items, 10, 20)
 
 """
 #print calculate bill function
@@ -52,10 +62,14 @@ print(brunch_menu.calculate_bill(['pancakes', 'home fries','coffee']))
 print(early_bird_menu.calculate_bill(['salumeria plate', 'mushroom ravioli (vegan)']))
 """
 
-menus = [brunch_menu, early_bird_menu, dinner_menu, kids_menu]
+menus = [brunch_menu, early_bird_menu, dinner_menu, kids_menu, arepas_menu]
 
 flagship_store = Franchise("1232 West End Road", menus)
 new_installment = Franchise("12 East Mulberry Stree", menus)
+arepas_place = Franchise("189 Fitzgerald Avenue", menus)
 
 #print(flagship_store.available_menus(14))
+
+Business = Business("Take a' Arepa", arepas_place)
+
 
